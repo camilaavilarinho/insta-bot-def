6a0386cafa3cfdb757c499dddfada402140b374c
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-12-20 21:19:39 +0000

    my updates

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,8 @@
 from instapy import InstaPy
+import os
 
-insta_username = ''
-insta_password = ''
+insta_username = os.environ.get('INSTA_USER') 
+insta_password = os.environ.get('INSTA_PW') 
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
@@ -9,14 +10,15 @@ session = InstaPy(username=insta_username, password=insta_password)
 session.login()
 
 # set up all the settings
-session.set_upper_follower_count(limit=2500)
-session.set_do_comment(True, percentage=10)
-session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-session.set_dont_include(['friend1', 'friend2', 'friend3'])
-session.set_dont_like(['pizza', 'girl'])
+#session.set_upper_follower_count(limit=2500)
+#session.set_do_comment(True, percentage=10)
+#session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+#session.set_dont_include(['friend1', 'friend2', 'friend3'])
+#session.set_dont_like(['pizza', 'girl'])
 
 # do the actual liking
-session.like_by_tags(['natgeo', 'world'], amount=100)
+#session.like_by_tags(['natgeo', 'world'], amount=100)
+session.follow_user_followers(['dublinveganhq'], amount=10, randomize=False)
 
 # end the bot session
 session.end()
